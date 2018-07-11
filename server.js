e852dc867b7bb6d274f5a2be855d5d4d7c5981e5
Olushi Luqman O <olushilukmon03@gmail.com>
2018-07-11 13:39:15 +0100

    handle root route

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', router);
 
+app.get('/', (req, res) => {
+  res.send(
+    `<p>Hello, world! Seems you need some/a pickup line(s). Get all <a href="/api">pickup lines</a> or a <a href="/api/random">random line</a></p>`
+  );
+});
+
 const server = app.listen(PORT, () => {
   const host = server.address().address;
   const port = server.address().port;
